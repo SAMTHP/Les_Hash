8add3a85ee126415bf30e0be766418ddc379935b
SAMTHP <samir_615@live.fr>
2018-10-04 16:34:36 +0200

    ELaboration du menu

--- journalist.rb
+++ journalist.rb
@@ -16,7 +16,7 @@
 # -- 0 -- Stockage des données du tableau dans un tableau "handles" :
 handles = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
 
-end
+
 
 ############################################################################################
 
@@ -24,11 +24,11 @@ end
 
 # Définition d'une méthode qui affichera le nombre d'index présent dans le tableau
 def nbre_journalist (array)
-	puts "Nombre d'adresses mail :"
-	puts array.length
+    puts "Nombre d'adresses mail :"
+    puts array.length
 end
 
-end
+
 
 ############################################################################################
 
@@ -36,15 +36,58 @@ end
 
 # Définition d'une méthode qui retourne les chiffres présent dans l'array Handles
 def nbre_numero (array)
-	return array.count("0-9")>0
+    return array.count("0-9")>0
 end
 
-end
 
 # Définition d'une méthode qui affichera le nombre de fois où un numéro est présent
-def resultat
-result = handles.select {|journalist|nbre_numero(journalist)}
-puts result.length
+def resultat_numero (array)
+    result1 = array.select {|journalist|nbre_numero(journalist)}
+    puts result1.length
+end
+
+############################################################################################
+
+# -- 3 -- Handle contenant les 4 lettres Aude à la suite
+
+# Définition d'une méthode qui calcul le nombre d'handle qui contiennent les 4 lettres du prénom "Aude" à la suite
+def nbre_aude (array)
+    auuude = array.grep(/(a|A)ude/)
+
+    puts auuude.length
+end
+
+############################################################################################
+
+# -- 4 -- Calcul du nombre d'handle qui commencent avec une majuscule
+
+# Définition de la méthode
+def begin_maj (array)
+   a = array.grep(/@\p{Lu}/)
+   compte = a.count
+   puts compte
+end
+
+############################################################################################
+
+# -- 5 -- Calcul du nombre d'handle qui contiennent une majuscule
+
+# Définition d'une méthode
+def nbre_maj (array)
+    maj = array.grep(/\p{Lu}/)
+    puts maj.length
+end
+
+############################################################################################
+
+# -- 6 -- Calcul du nombre d'underscore dans tous les handles confondus
+
+# Définition d'une méthode
+def nbre_under (array)
+    under1 = array.grep(/_/)
+    under2 = array.grep(/_.*_/)
+    compte = under1 + under2 
+    puts compte.count
 end
 
 ############################################################################################
@@ -53,11 +96,25 @@ end
 
 # Définition d'une méthode qui range par ordre alphabétique les index du tableau handles
 def trie (array)
-	puts "Noms journalistes rangées par ordre alphabétique :"
-	puts array.sort
+    puts "Noms journalistes rangées par ordre alphabétique :"
+    puts array.sort
 end
 
-end
+
 ############################################################################################
 
-resultat
+# -- BIS -- Simulation complète
+
+# Définition de la méthode perform
+def perform
+    puts "Listes des questions"
+    puts "1 - Combien y a t'il de journalistes dans cet array ?"
+    puts "2 - Combien d'handle contiennent un numéro ?"
+    puts "3 - Combien d'handle contiennent les 4 lettres du prénom \"Aude\" à la suite (sans faire attention à la casse) ?"
+    puts "4 - Combien commencent par une majuscule ?"
+    puts "5 - Combien contiennent une majuscule ?"
+    puts "6 - Combien de _ dans tous les pseudos confondus ?"
+    puts "7 - Trie la liste de handle par ordre alphabétique"
+end
+
+perform
